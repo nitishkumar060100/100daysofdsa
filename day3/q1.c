c4cccd0c7f7e332937a558eae1eb1264c6fbb2f3
nitishkumar060100 <nitishkumar060100@gmail.com>
2026-02-11 15:38:05 +0530

    day3q1.c

---
+++ day3/q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, k;
+    
+    // Input array size
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    
+    scanf("%d", &k);
+    
+    int comparisons = 0;
+    int found = -1;
+    
+    
+    for(i = 0; i < n; i++) {
+        comparisons++;           
+        if(arr[i] == k) {
+            found = i;
+            break;
+        }
+    }
+    
+    if(found != -1) {
+        printf("Found at index %d\n", found);
+    } else {
+        printf("Not Found\n");
+    }
+    
+    printf("Comparisons = %d\n", comparisons);
+    
+    return 0;
+}
