58435c60e0750a808a92225ec3c1df4e57c320bd
nitishkumar060100 <nitishkumar060100@gmail.com>
2026-04-13 17:18:42 +0530

    .

---
+++ days71/q1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY -1
+
+int table[1000];
+
+int main() {
+    int m, q;
+    scanf("%d", &m);
+    scanf("%d", &q);
+
+    for (int i = 0; i < m; i++)
+        table[i] = EMPTY;
+
+    while (q--) {
+        char op[10];
+        int key;
+        scanf("%s %d", op, &key);
+
+        int h = key % m;
+
+        if (strcmp(op, "INSERT") == 0) {
+            for (int i = 0; i < m; i++) {
+                int idx = (h + i * i) % m;
+                if (table[idx] == EMPTY) {
+                    table[idx] = key;
+                    break;
+                }
+            }
+        } else if (strcmp(op, "SEARCH") == 0) {
+            int found = 0;
+            for (int i = 0; i < m; i++) {
+                int idx = (h + i * i) % m;
+                if (table[idx] == key) {
+                    found = 1;
+                    break;
+                }
+                if (table[idx] == EMPTY)
+                    break;
+            }
+            if (found)
+                printf("FOUND\n");
+            else
+                printf("NOT FOUND\n");
+        }
+    }
+
+    return 0;
+}
